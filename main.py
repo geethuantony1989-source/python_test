dd922a622d8de9e89f778eadf63574128f579c4b
geethuantony1989-source <geethuantony1989@gmail.com>
2025-09-17 11:04:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Test passed')
+        print('PASS')
     else:
-        print('Test failed')
+        print('FAIL')
 
 in_autotests_we_trust(10, '10')
 
 in_autotests_we_trust(0, False)
 in_autotests_we_trust(1, False)
 in_autotests_we_trust(20, '220')
+in_autotests_we_trust(20, '20')
